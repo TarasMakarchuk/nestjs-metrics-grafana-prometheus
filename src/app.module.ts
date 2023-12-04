d65b7155bea0f005bf20f23bfae88e6385eca686
Taras Makarchuk <2278452@gmail.com>
2023-12-04 23:37:40 +0200

    feat: add logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 @Module({
   imports: [PrometheusModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor,
+  }],
 })
 export class AppModule {}
